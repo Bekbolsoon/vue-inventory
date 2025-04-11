552c3348a95dea323737d62ade40fbaf02e010a9
Bekbolsun Matisakov <matisakov66@mail.ru>
2025-04-11 16:35:56 +0600

    Добавлен временный блок

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,26 @@
   <div class="app__wrapper">
     <Profile />
     <Inventory />
+    <TempBlock />
   </div>
 </template>
 
 <script setup lang="ts">
 import Profile from "@/components/Profile.vue"
 import Inventory from "@/components/Inventory.vue";
+import TempBlock from "@/components/TempBlock.vue";
 </script>
 
 <style lang="scss" scoped>
+.app {
+  margin: 0 auto;
+}
 .app__wrapper {
   max-width: 849px;
   padding: 32px;
   display: grid;
   grid-template-columns: 236px 1fr;
   column-gap: 24px;
+  row-gap: 24px;
 }
 </style>
---
+++ src/components/TempBlock.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="temp-block" v-if="!isClosedTempBlock">
+    <div class="temp-block__skeleton skeleton-text"></div>
+    <img
+      @click="isClosedTempBlock = true"
+      class="temp-block__close-icon"
+      src="../assets/img/close-modal.svg"
+      alt="close-icon">
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const isClosedTempBlock = ref(false)
+</script>
+
+<style lang="scss" scoped>
+.temp-block {
+  grid-column: span 2;
+  padding: 18px 68px 18px 18px;
+  background: #262626;
+  border: 1px solid #4d4d4d;
+  border-radius: 12px;
+  position: relative;
+}
+
+.temp-block__skeleton {
+  height: 36px;
+  border-radius: 12px;
+}
+
+.temp-block__close-icon {
+  position: absolute;
+  right: 14px;
+  top: 14px;
+  cursor: pointer;
+}
+</style>
